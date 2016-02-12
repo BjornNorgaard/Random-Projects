375dbb0576526aafd69c84837e9fb249b3921453
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-02-12 17:51:53 +0100

    program for bookmarks in txt file, needs gui

---
+++ TXT Bookmarks/TXT Bookmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TXT_Bookmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("readthis.txt");
+          
+            Debug.WriteLine("File contains these sites:");
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("http"))
+                {
+                    Debug.WriteLine("\t" + line);
+                    Process.Start("chrome.exe", "--incognito " + line.ToString());
+                }
+            }
+        }
+    }
+}
