3a1b949aeafa629fca65c60aebec186efd33ca38
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-03-03 23:52:14 +0100

    better user interface, no need to filename anymore

---
+++ FileOperations/FolderOperations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace FolderOperations
+{
+    class Program
+    {
+        public static void EncryptFile(string pathToFile)
+        {
+            System.IO.File.Encrypt(path: pathToFile);
+        }
+
+        public static void DecryptFile(string pathToFile)
+        {
+            System.IO.File.Decrypt(path: pathToFile);
+        }
+
+        static void Main(string[] args)
+        {
+            EncryptFile("file.txt");
+            Console.WriteLine("done encrypting");
+
+            Console.WriteLine("waiting a bit...");
+            Thread.Sleep(5000);
+
+            Console.WriteLine("beginning decryption");
+            DecryptFile("file.txt");
+        }
+    }
+}
--- TXT Bookmarks/TXT Bookmarks/Program.cs
+++ TXT Bookmarks/TXT Bookmarks/Program.cs
@@ -6,9 +6,12 @@ namespace TXT_Bookmarks
 {
     class Program
     {
-        public static void OpenSitesInThisFile(string file)
+        static string[] files = { };
+        static string[] lines = { };
+
+        public static void OpenSitesInThisFile(int fileToOpen)
         {
-            string[] lines = System.IO.File.ReadAllLines(file);
+            lines = System.IO.File.ReadAllLines(files[fileToOpen]);
 
             //Debug.WriteLine("File contains these sites:");
 
@@ -22,9 +25,9 @@ namespace TXT_Bookmarks
             }
         }
 
-        public static void SplitFileIntoSmallerFiles(string file, int numberOfFilesToSplitInto)
+        public static void SplitFileIntoSmallerFiles(int fileToOpen, int numberOfFilesToSplitInto)
         {
-            string[] lines = System.IO.File.ReadAllLines(file);
+            string[] lines = System.IO.File.ReadAllLines(files[fileToOpen]);
 
             int nameNumberForFile = 0;
             for (int i = 0; i < lines.Length; i++)
@@ -45,6 +48,23 @@ namespace TXT_Bookmarks
             }
         }
 
+        private static void PrintFilesInCurrentFolder()
+        {
+            files = System.IO.Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+            }
+
+            //Console.WriteLine("\n");
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                Console.WriteLine("\t" + "(" + i + ") " + file);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("What do u want to do?" +
@@ -53,22 +73,32 @@ namespace TXT_Bookmarks
             Console.Write("Choice: ");
 
             var choice = Console.ReadLine();
+            Console.Clear();
 
             if (choice != null && int.Parse(choice) == 1)
             {
-                Console.Write("\t Open all sites in this file: ");
-                string file = Console.ReadLine();
+                Console.WriteLine("You can open one of these files:");
 
-                OpenSitesInThisFile(file);
+                PrintFilesInCurrentFolder();
+
+                Console.Write("Choice: ");
+                //Console.Write("\nOpen all sites in this file: ");
+                int numberOfFileToOpen = int.Parse(Console.ReadLine());
+
+                OpenSitesInThisFile(numberOfFileToOpen);
             }
             else if (choice != null && int.Parse(choice) == 2)
             {
-                Console.Write("\t Which file do you want to split: ");
-                string file = Console.ReadLine();
+                Console.WriteLine("You can split one of these files:");
+                PrintFilesInCurrentFolder();
 
-                Console.Write("\t How many file should it be split into: ");
+                Console.Write("Choice: ");
+                int file = int.Parse(Console.ReadLine());
+
+                Console.Write("How many file should it be split into: ");
+                Console.Write("\nChoice: ");
                 int numberOfFileToSplitItInto = int.Parse(Console.ReadLine());
-
+                
                 SplitFileIntoSmallerFiles(file, numberOfFileToSplitItInto);
             }
 
