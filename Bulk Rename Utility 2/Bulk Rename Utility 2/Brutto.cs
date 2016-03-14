8819082451a62ed464a6c13664a0d24e8c136b5c
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-03-14 18:46:36 +0100

    initial commit

---
+++ Bulk Rename Utility 2/Bulk Rename Utility 2/Brutto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulk_Rename_Utility_2
+{
+    public class Brutto
+    {
+        public void RenameFile(string filename)
+        {
+
+        }
+    }
+}
