f4796566062373181714d2a421ba62946bb4d27f
Bjørn Tower <bjornsnorgaard@gmail.com>
2016-03-14 19:06:15 +0100

    created methods renamefile and renameallfilesinfolder

--- Bulk Rename Utility 2/Bulk Rename Utility 2/Brutto.cs
+++ Bulk Rename Utility 2/Bulk Rename Utility 2/Brutto.cs
@@ -8,9 +8,20 @@ namespace Bulk_Rename_Utility_2
 {
     public class Brutto
     {
-        public void RenameFile(string filename)
+        public void RenameFile(string oldFilename, string newFilename)
         {
+            System.IO.File.Move(oldFilename, newFilename);
+        }
 
+        public void RenameAllFilesInFolder(string folder)
+        {
+            // Load filenames into stringarray
+            string[] files = System.IO.Directory.GetFiles(folder);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                RenameFile(files[i], "derpfile" + i);
+            }
         }
     }
 }
--- Bulk Rename Utility 2/BulkRenamer.Application/Program.cs
+++ Bulk Rename Utility 2/BulkRenamer.Application/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,12 @@ namespace BulkRenamer.Application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            //System.IO.File.Create(@"C:\Users\Norgaard\Downloads\Hello.txt");
+
+            //System.IO.File.Move(@"C:\Users\Norgaard\Downloads\Hello.txt", @"C:\Users\Norgaard\Downloads\Derp.txt");
+
+            
+            Console.WriteLine("" + Directory.GetCurrentDirectory());
         }
     }
 }
