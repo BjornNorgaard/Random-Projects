dfd159002a32c0f046af4ab68c5a463c4198b1af
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2016-12-04 12:36:07 +0100

    started project and created class for commns

---
+++ Procalender16/Procalender/PShopClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Procalender
+{
+    public class PShopClient
+    {
+        public static HttpClient Client { get; set; }
+
+        public PShopClient()
+        {
+            // http://jul.proshop.dk/Home/SubmitQuestion?Length=4
+            Client.BaseAddress = new Uri("http://jul.proshop.dk/Home/SubmitQuestion?Length=4");
+        }
+
+        public bool PostAll()
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Hello, this went wrong: {e}");
+            }
+
+            return false;
+        }
+    }
+}
