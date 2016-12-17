546383f86c6427544f3994006581860c05dd990e
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2016-12-17 22:40:44 +0100

    problem 16 done

---
+++ Problem016/Problem016/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger numbers = (BigInteger)Math.Pow(2, 1000);
+            string text = numbers.ToString();
+            var sum = 0;
+
+            foreach (char c in text)
+            {
+                sum += c - '0';
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
